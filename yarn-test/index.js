522179d9aa0bf7477b5fce79135e13787f9e4f33
tbswang <gt2017qd@163.com>
2021-07-07 16:22:10 +0800

    单独安装

--- yarn-test/index.js
+++ yarn-test/index.js
@@ -1,4 +1,5 @@
 var qs = require('qs');
 
-var obj = qs.parse('a=c');
-console.log(Object.keys(obj).join(','))
+// var obj = qs.parse('a=c');
+// console.log(Object.keys(obj).join(','))
+console.log(qs.stringify({ a: ['b', 'c'] }, { arrayFormat: 'comma' }))
